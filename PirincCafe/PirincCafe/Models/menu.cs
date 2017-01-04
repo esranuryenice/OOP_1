c0d96b8ff25453a3ab868ff8679eba5dd76918dd
esranur.yenice <esranur.yenice@ALS-YG39104-J01>
2017-01-04 11:51:06 +0200

    Ödev

---
+++ PirincCafe/PirincCafe/Models/menu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PirincCafe.Models
+{
+    class menu
+    {
+        public List<AnaYemek> anayemekler { get; set; }
+        public List<Salata> salatalar { get; set; }
+        public List<Tatli> tatli { get; set; }
+    }
+}
